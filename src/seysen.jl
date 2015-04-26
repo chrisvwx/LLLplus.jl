9edd26376dec81077dbfbc0855c0e0c22f0f3e59
Chris Peel <chris.peel@ieee.org>
2015-04-26 12:57:58 -0700

    Updated tests to include more from latticechallenge.org

--- src/seysen.jl
+++ src/seysen.jl
@@ -1,46 +1,46 @@
 function seysen{Td}(H::Array{Td,2})
-# [T,H_red,H_red_dual,num_it] = seysen{Td}(H::Array{Td,2})
+# [B,T,B_dual,num_it] = seysen{Td}(H::Array{Td,2})
 #
 # Do greedy Seysen lattice reduction on the matrix H (real or
 # complex-valued), returning T a unimodular matrix that reduces H;
-# H_red, the reduced lattice basis (i.e. H_red = H*T);
-# H_red_dual,  dual lattice basis (i.e., H_red_dual = pinv(H_red)); and
+# B, the reduced lattice basis (i.e. B = H*T);
+# B_dual,  dual lattice basis (i.e., B_dual = pinv(B)); and
 # the number of iterations (the number of basis updates).
 
 # Follows LLL in D. Wueben, et al, MMSE-Based Lattice-Reduction for Near-ML
 # Detection of MIMO Systems International IEEE Workshop on Smart Antennas,
 # Munich, March 2004.
 
+if Td<:BigInt || Td<:BigFloat
+    Ti= Td<:Complex?Complex{BigInt}:BigInt
+else
+    Ti= Td<:Complex?Complex{Int}:Int
+end
+roundf(r) = Td<:Complex?round(real(r)) + im*round(imag(r)):round(r);
+
+#H = H*1.0
+
 # get size of input
 (n, m) = size(H); # m-dimensional lattice in an n-dimensional space
 
-if isempty(H)
-    error("Input basis is empty");
-elseif rank(H) < m
-    error("Input basis has not full column rank");
-end
-Ti = Td<:Complex? Complex{Int}: Int; # Integer type: Complex or not
-Tf = Td<:Complex? Complex{FloatingPoint}: FloatingPoint;
-H = convert(Array{Tf,2},H);
-
 # initialization, outputs
-H_red      = copy(H);    # reduced lattice basis
-num_it     = 0;          # number of iterations
-T          = eye(Ti,m);  # unimodular matrix
-A          = H'*H;       # Gram matrix of H
-Adual      = inv(A);     # Inverse gram matrix of H
-H_red_dual = H*Adual;    # Dual basis
+B      = copy(H);    # reduced lattice basis
+num_it = 0;          # number of iterations
+T      = eye(Ti,m);  # unimodular matrix
+A      = (H'*H)*1.0;       # Gram matrix of H
+Adual  = inv(A);     # Inverse gram matrix of H
+B_dual = H*Adual;    # Dual basis
 
 # calculate all possible update values Λ[s,t)
 # and their corresponding reduction Δ[s,t) in Seysen's measure
-Λ  = zeros(Ti,m,m);
-Δ   = zeros(Tf,m,m);
+Λ = zeros(Ti,m,m);
+Δ = zeros(Td,m,m)*0.0;
 
 for s = 1:m
     for t = 1:m
         if s != t
 	    x = 0.5*(Adual[t,s]/Adual[s,s]-A[t,s]/A[t,t]);
-            Λ[s,t] = round(real(x))+ im*round(imag(x));
+            Λ[s,t] = roundf(x);
 	    AbsΛ   = abs(Λ[s,t])^2;
 	    if AbsΛ != 0
 	        zw = real(Λ[s,t])*real(x)+imag(Λ[s,t])*imag(x);
@@ -68,13 +68,13 @@ while do_reduction
     num_it = num_it + 1;
     
     # perform basis update
-    H_red[:,s] = H_red[:,s] + Λ[s,t]*H_red[:,t];
+    B[:,s] = B[:,s] + Λ[s,t]*B[:,t];
 
     # compute corresponding unimodular trasformation matrix
     T[:,s] = T[:,s] + Λ[s,t]*T[:,t];  # basis transformation matrix
 
     # update corresponding dual basis
-    H_red_dual[:,t] = H_red_dual[:,t] - Λ[s,t]'*H_red_dual[:,s];
+    B_dual[:,t] = B_dual[:,t] - Λ[s,t]'*B_dual[:,s];
 
     # Update Gram and inverse Gram matrix
     for ind = 1:m
@@ -84,7 +84,7 @@ while do_reduction
             A[s,ind] = A[s,ind]+Λ[s,t]'*A[t,ind];
             A[ind,s] = A[s,ind]';
         else
-            A[s,ind] = norm(H_red[:,s]).^2;
+            A[s,ind] = norm(B[:,s]).^2;
         end
 
         # update inverse Gram matrix
@@ -92,7 +92,7 @@ while do_reduction
             Adual[t,ind] = Adual[t,ind]-Λ[s,t]*Adual[s,ind];
             Adual[ind,t] = Adual[t,ind]';
         else
-            Adual[t,ind] = norm(H_red_dual[:,t])^2;
+            Adual[t,ind] = norm(B_dual[:,t])^2;
         end
 
     end # - end update Gram und inverse Gram matrix
@@ -104,8 +104,17 @@ while do_reduction
             if ((ind1==s) | (ind1==t) | (ind2==s) | (ind2==t)) & (ind1!=ind2)
                 x = 0.5*(Adual[ind2,ind1]/Adual[ind1,ind1]-A[ind2,ind1]/
                          A[ind2,ind2]);
-                #            Λ[ind1,ind2] = round(x);
-                Λ[ind1,ind2] = round(real(x))+ im*round(imag(x));
+                try
+                    Λ[ind1,ind2] = roundf(x);
+                catch
+                    println("x = $(x), typeof(x) = $(typeof(x))")
+                    println("A[ind2,ind2] = $(A[ind2,ind2]), typeof(A[ind2,ind2]) = $(typeof(A[ind2,ind2]))")
+                    println("roundf(x)) = $(roundf(x))"*
+                            "typeof(roundf(x))) = $(typeof(roundf(x)))")
+                    println("Λ[ind1,ind2]) = $(Λ[ind1,ind2])"*
+                            "typeof(Λ[ind1,ind2])) = $(typeof(Λ[ind1,ind2]))")
+                    println(" ")
+                end
                 AbsΛ = abs(Λ[ind1,ind2])^2;
                 if AbsΛ != 0
                     zw = real(Λ[ind1,ind2])*real(x)+imag(Λ[ind1,ind2])*imag(x);
@@ -129,5 +138,5 @@ while do_reduction
 end # - end lattice reduction loop
 
 
-return (T,H_red,H_red_dual,num_it)
+return (B,T,B_dual,num_it)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,32 +7,64 @@ using LLLplus
 using Base.Test
 
 # --------------
-# unit tests
+# tests with small matrices
 # --------------
+println("tests with small matrices\n")
 
-# From http://home.ie.cuhk.edu.hk/~wkshum/wordpress/?p=442
-H =[1     9     1     2;
-    1     8     8     3;
-    7     4     5     1;
-    2     6     7     1];
-B1=[ 2   3  -2  -4
-     3  -1   2   1
-     1   1   6  -4
-     1   3  -1   3];
-T1=[ 0     0     1    -1
-     0     1    -1     0
-     0     0     0     1
-     1    -3     3    -2];
+# Matrix from http://home.ie.cuhk.edu.hk/~wkshum/wordpress/?p=442
+H =[1   9   1   2;
+    1   8   8   3;
+    7   4   5   1;
+    2   6   7   1];
+B1=[2   3  -2  -4
+    3  -1   2   1
+    1   1   6  -4
+    1   3  -1   3];
+T1=[0   0   1  -1
+    0   1  -1   0
+    0   0   0   1
+    1  -3   3  -2];
 (B,T) = lll(H);
 @test sum(abs(B-B1))==0
 @test sum(abs(T-T1))==0
 
+# --------------
+# test norm for matrix from http://www.latticechallenge.org/
+# --------------
+m=200
+println("Testing now with $(m)x$(m) matrix from latticechallenge.org.")
+println("All the column norms should be 30.")
+mat = readdlm("challenge-$(m).mod",Int64)
+
+nrms = zeros(m,1)
+for ix=1:m
+    nrms[ix] = norm(mat[:,ix])
+end
+println("max col-norm of input is $(maximum(nrms))")
+
+@time (B,T) = lll(mat);
+nrms = zeros(m,1)
+for ix=1:m
+    nrms[ix] = norm(B[:,ix])
+end
+maxlll = maximum(nrms)
+println("max col-norm of lll-reduced basis is $(maxlll)")
+@test maxlll<=30+1e-6
+
+@time (B,T) = seysen(mat);
+nrms = zeros(m,1)
+for ix=1:m
+    nrms[ix] = norm(B[:,ix])
+end
+maxSeysen = maximum(nrms)
+println("max column norm of seysen-reduced basis is $(maxSeysen)")
+@test maxSeysen<=30+1e-6
 
 # --------------
 # Run tests with random matrices
 # --------------
 
-println("In all the following tests, the first time includes the "*
+println("\nIn all the following tests, the first time includes the "*
         "JIT compilation; \nfor the second execution the "*
         "compilation is already done and the time\n"*
         "should be faster.\n")
@@ -47,21 +79,21 @@ println("Testing LLL on $(N)x$(N) real matrix...")
 # Time LLL, Seysen, VBLAST decompositions of a 10x10 complex matrix with
 # randn entries
 N = 10;
+println("\nTesting LLL on $(N)x$(N) complex matrix...")
 H = randn(N,N) + im*randn(N,N);
-println("Testing LLL on $(N)x$(N) complex matrix...")
 @time (B,T,Q,R) = lll(H);
 @time (B,T,Q,R) = lll(H);
-println("Testing Seysen on $(N)x$(N) complex matrix...")
-@time (T,H_red,H_red_dual) = seysen(H);
-@time (T,H_red,H_red_dual) = seysen(H);
-println("Testing VBLAST on $(N)x$(N) complex matrix...")
+println("Testing Seysen on same $(N)x$(N) complex matrix...")
+@time (B,T) = seysen(H);
+@time (B,T) = seysen(H);
+println("Testing VBLAST on same $(N)x$(N) complex matrix...")
 @time (W,P,B) = vblast(H);
 @time (W,P,B) = vblast(H);
 
 # Test sphere decoder
 Ns = 100000;
 N = 2;
-println("Testing sphere decoder on $(Ns) samples of $(N)x$(N) BPSK system...")
+println("\nTesting sphere decoder on $(Ns) samples of $(N)x$(N) BPSK system...")
 NN = randn(N,Ns)/sqrt(100);
 H = [.6 -2.6; -.5 .95];
 C = [-1,1];
@@ -72,4 +104,4 @@ Y = H*X+NN;
 @time Zt = hard_sphere(Y,H,2);
 Zh = Zt+1;
 errRate = sum(abs(Z-Zh))/Ns;
-println("Error Rate is $(errRate). It should be zero or very small.")
+println("Error Rate is $(errRate). It should be zero or very small.\n")
