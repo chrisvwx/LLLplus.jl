0ec002e60d7cf6b7de656380b96a54e8b3d1211b
Christian Peel <chris.peel@ieee.org>
2018-08-25 20:41:12 -0700

    Updated benchmarks and README to reflect recent updates.

--- benchmark/perftest.jl
+++ benchmark/perftest.jl
@@ -1,21 +1,14 @@
-if VERSION<=v"0.6.3"
-    include("../src/LLLplus.jl")
-else
-    using Pkg
-end
-
-Pkg.add("PyPlot")
-Pkg.add("BenchmarkTools")
-Pkg.add("Docile")
-
 using PyPlot
 using BenchmarkTools
 using LLLplus
+using Printf
+using Random
+using LinearAlgebra
 
 include("lrtest.jl")
 
-lrtest(5,2.^[2],[100],[Int32,Int64,Int128,Float64,BigInt,BigFloat],"rand")
+lrtest(5,2 .^[2],[100],[Int32,Int64,Int128,Float64,BigInt,BigFloat],"rand")
 savefig("benchmark/perfVsDataTypeN16.png") # run from root directory
 
-lrtest(5,2.^[0:5;],[1],[Float64],"randn")
+lrtest(5,2 .^[0:5;],[1],[Float64],"randn")
 savefig("benchmark/perfVsNfloat32.png") # run from root directory
