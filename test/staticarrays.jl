139b10693a5075cde41397f916133b62568f87c3
Chris Peel <chris.peel@ieee.org>
2024-11-28 15:44:05 -0800

    cleanup, rebase for static arrays

--- test/staticarrays.jl
+++ test/staticarrays.jl
@@ -6,7 +6,6 @@ using StaticArrays, Test
          0.9766849678971312 0.6302875815478448 0.4788314270985644]
     sH = SMatrix{3, 3, Float64, 9}(H)
     @test lll(H)[1] ≈ lll(sH)[1]
-    @test l2(H)[1] ≈ l2(sH)[1]
     @test brun(H)[1] ≈ brun(sH)[1]
     @test seysen(H)[1] ≈ seysen(sH)[1]
 end
